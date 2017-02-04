6ec2926116d64b41a67c68f64b5f966252ef503f
SergeiKSS <SergeiKSS@mail.ru>
2017-02-04 10:36:26 +0200

    doubly-linked-list:implement last test

--- src/linked-list.js
+++ src/linked-list.js
@@ -18,6 +18,7 @@ class LinkedList {
             this._tail = node;
         }
         this.length++;
+        return this;
     }
 
     head() {return this._head.data;}
@@ -39,6 +40,10 @@ class LinkedList {
         var curentNode = this._head,
             length = this.length,
             count = 0;
+        if (curentNode==null){
+            this.append(data);
+            return this;
+        }
         while (count < index){
             curentNode = curentNode.next;
             count++;
@@ -55,21 +60,34 @@ class LinkedList {
     }
 
     clear() {
+        if (this.isEmpty()){
+            return this;} else {
         this._head.data = null;
         this._tail.data = null;
         this.length = 0;
+        return this;}
     }
 
     deleteAt(index) {
         var curentNode = this._head,
             length = this.length,
             count = 0;
+        if (index==0 || length==1){
+        this._head = null;
+        this._head = null;
+        this._tail = null;
+        this.length = length -1;
+        return this;
+         }    
         while (count < index){
             curentNode = curentNode.next;
             count++;
         }
+        curentNode.data = null;
         curentNode.next.prev = curentNode.prev;
         curentNode.prev.next = curentNode.next;
+        this.length = length -1;
+        return this;
     }
 
     reverse() {
@@ -80,7 +98,9 @@ class LinkedList {
             node.prev = this._tail.next;
             this._tail.data = this._head.data;
             this._head = node;
-
+        if (length==1){
+                return this;
+            }       
         while (count < length){
             var temp = node.next;
             temp.next = temp.prev;
@@ -88,6 +108,7 @@ class LinkedList {
             node = temp;
             count++;
         }
+        return this;
     }
 
     indexOf(data) {
